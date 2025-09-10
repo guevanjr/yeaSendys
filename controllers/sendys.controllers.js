b71907555fcf930c15c2e632cf8f363ed784b165
Simiao Guevane <guevanjr@gmail.com>
2025-09-10 16:29:09 +0200

    Sendys CRM Testing

--- controllers/sendys.controllers.js
+++ controllers/sendys.controllers.js
@@ -172,6 +172,11 @@ exports.insertCustomer = async function (req, res) {
 */
             var userRequest = '<?xml version="1.0" encoding="utf-8"?>\
             <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">\
+            <soap:Header>\
+                <AuthenticationHeader xmlns="capgemini/crm/webservices/cliente">\
+                <TokenId>' + token + '</TokenId>\
+                </AuthenticationHeader>\
+            </soap:Header>\
             <soap:Body>\
                 <InserirCliente xmlns="capgemini/crm/webservices/cliente">\
                 <cliente>\
