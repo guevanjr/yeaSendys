5939e900a05e6cf48f27793a2c614eb29538a20d
Simiao Guevane <guevanjr@gmail.com>
2025-09-16 08:18:40 +0200

    Improvements in CRM Integration

--- controllers/sendys.controllers.js
+++ controllers/sendys.controllers.js
@@ -15,7 +15,7 @@ var tokenRequest = '<?xml version="1.0" encoding="utf-8"?>\
 </soap:Envelope>';
 
 async function addContacts (customerId, customerName, email, token, phone, mobile) {           
-    var userUrl = 'http://crm.aqi.co.mz/SendysCRM/webservices/Tarefa.asmx';
+    var userUrl = 'http://crm.aqi.co.mz/SendysCRM/webservices/Cliente.asmx';
     var userRequest = '<?xml version="1.0" encoding="utf-8"?>\
     <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">\
     <soap:Header>\
