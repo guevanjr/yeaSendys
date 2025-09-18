62f66c2791bb7b47e8d9d72d3b5a1151d31d9313
Simiao Guevane <guevanjr@gmail.com>
2025-09-18 11:55:50 +0200

    Sendys Integration

--- controllers/sendys.controllers.js
+++ controllers/sendys.controllers.js
@@ -204,7 +204,7 @@ exports.getCustomer = async function (req, res) {
                     // Check if user exists in database
                     if (result) {
                         // User exists - return URL with pre-filled data
-                        var data = result['soap:Envelope']['soap:Body']['QueryByPhoneResponse']['QueryByPhoneResult']['ContactosNoCliente']['ContactoNoCliente_Data'];
+                        var data = result['soap:Envelope']['soap:Body']['QueryByPhoneResponse']['QueryByPhoneResult']['ContactosNoCliente']['ContactoNoCliente_Data'].IdCliente;
                         console.log('Call Result: ' + data/*result['soap:Envelope']['soap:Body']['QueryByPhoneResponse']['QueryByPhoneResult']['ContactosNoCliente']['ContactoNoCliente_Data'][0]*/);
                         res.send({ data: data /*result['soap:Envelope']['soap:Body'].QueryByPhoneResponse.QueryByPhoneResult.ContactosNoCliente.*/ });
                         /*
