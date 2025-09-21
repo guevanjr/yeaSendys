7cb237987cba68bf41c242e03c49e46f2b3efbfc
Simiao Guevane <guevanjr@gmail.com>
2025-09-21 15:39:16 +0200

    Sendys CRM Integration

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const axios = require('axios');
 const port = process.env.PORT || 7225;
 const app = express();
+const xmlparser = require('express-xml-bodyparser'); // You'd need to install this package
 
+// Use xmlparser for XML payloads
+app.use(xmlparser());
 app.use(express.json()); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
